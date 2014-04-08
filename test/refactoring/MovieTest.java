f7d6461cac511f797a2635bb205ccc36d3b780c4
moloch <dario.coco@gmail.com>
2014-04-08 14:35:10 +0200

    1st green bar for MovieTest

--- test/refactoring/MovieTest.java
+++ test/refactoring/MovieTest.java
@@ -6,27 +6,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MovieTest {
-	
+
 	private Movie movie;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		movie = new Movie("Gran Torino", 666);
 	}
 
 	@Test
 	public void testGetPriceCode() {
-		assertEquals(666,movie.getPriceCode());
+		assertEquals(666, movie.getPriceCode());
 	}
 
 	@Test
 	public void testSetPriceCode() {
-		fail("Not yet implemented");
+		assertEquals(666, movie.getPriceCode());
+		movie.setPriceCode(777);
+		assertEquals(777, movie.getPriceCode());
 	}
 
 	@Test
 	public void testGetTitle() {
-		fail("Not yet implemented");
+		assertEquals("Gran Torino", movie.getTitle());
 	}
 
 }
