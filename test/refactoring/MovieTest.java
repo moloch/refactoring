2f633cdd12d144c80d81c9aee9f2bb7ef49a5ea1
moloch <orione-team@xpeppers.com>
2014-04-08 14:24:33 +0200

    initial import

---
+++ test/refactoring/MovieTest.java
@@ -0,0 +1,29 @@
+package refactoring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieTest {
+
+	@Test
+	public void testMovie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPriceCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPriceCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+}
