d5b0bd9773ba9adef3b9aaead90f350d77104e9e
moloch <dario.coco@gmail.com>
2014-04-09 14:35:35 +0200

    Added Customer, test for Rental

---
+++ test/refactoring/RentalTest.java
@@ -0,0 +1,19 @@
+package refactoring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RentalTest {
+
+	@Test
+	public void testGetDaysRented() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMovie() {
+		fail("Not yet implemented");
+	}
+
+}
